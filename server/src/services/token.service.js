52b12fbd17db3f7860490f060b46b3b18d99ec02
emantrader49-art <emantrader49@gmail.com>
2026-09-06 17:40:10 +0500

    Update token.service.js

--- server/src/services/token.service.js
+++ server/src/services/token.service.js
@@ -25,7 +25,7 @@ export function refreshCookieOptions() {
   return {
     httpOnly: true,
     secure: env.NODE_ENV === "production",
-    sameSite: "lax",
+   sameSite: env.NODE_ENV === "production" ? "none" : "lax",
     path: "/api/auth",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   };
